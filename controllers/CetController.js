1f31bb17107f2f8bffdc2486f9b6f506d99735d4
Vitor Paulo <89135117+vitorpj1@users.noreply.github.com>
2022-04-29 10:19:32 -0300

    cld,cet,tltc

---
+++ controllers/CetController.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const path = require("path")
+const ejs = require("ejs");
+const puppeteer = require("puppeteer");
+app.set("view engine","ejs");
+
+const cliente = [
+    {
+        nome:"teste",
+        valor:"1",
+        cpf:"1",
+        taxa:"1",
+    }
+        
+]
+
+router.get("/create-cet",(request,response)=>{
+     response.render("cet");
+ })
+ router.post("/save-cet",(request,response)=>{
+     const dataExtrato = request.body;
+     cliente.push(dataExtrato);
+     response.redirect("/ncet");
+ })
+ 
+ router.get("/ncet",(request,response)=>{
+     const filePath = path.join(__dirname, "../src/print-cet.ejs");
+ 
+    
+     ejs.renderFile(filePath,{cliente},(err,html)=>{
+             if(err){
+                 return response.send(err)
+             }else{
+                 //Enviar para o navegador
+                 return  response.send(html); 
+             }                         
+         })
+ })
+ 
+ router.get("/cet", async(request,response)=>{
+    var hostname = request.headers.host;
+
+
+     const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+ 
+     const page = await browser.newPage();
+     
+    if(hostname == "localhost:3001"){
+        await page.goto("http://localhost:3001/ncet",{
+            waitUntil:['domcontentloaded', 'networkidle0']
+     })
+    }else{
+    await page.goto("https://gerador-pdf.herokuapp.com/ncet",{
+            waitUntil:['domcontentloaded', 'networkidle0']
+     })
+    }
+ 
+     
+ 
+     const pdf = await page.pdf({
+         printBackground:true,
+         format:"a4",
+         width:"8.27in",
+         height:"11.7in"
+     })
+ 
+     await browser.close();
+ 
+     response.contentType("application/pdf")
+     
+     return response.send(pdf);
+ })
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-
+//Comando para subir aplicacao no HEROKU
+// git push heroku main 
 const express = require("express");
 const app = express();
 
@@ -13,6 +14,9 @@ const tedController = require("../controllers/TedController");
 const protocoloController = require("../controllers/ProtocoloController");
 const iofController = require("../controllers/IofController");
 const ddaController = require("../controllers/DdaController");
+const cldController = require("../controllers/CldController");
+const cetController = require("../controllers/CetController");
+//const tltcController = require("../controllers/TltcController");
 
 //view engine
 app.set("view engine","ejs");
@@ -28,6 +32,9 @@ app.use("/",protocoloController);
 app.use("/",tedController);
 app.use("/",contratoController);
 app.use("/",ddaController);
+app.use("/",cldController);
+app.use("/",cetController);
+//app.use("/",tltcController);
 app.get("/",(request,response)=>{
     response.render("index")
 })
